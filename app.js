4dc1b25918414725398d259d9e952814d5c09482
tibraj <tibraj94@gmail.com>
2021-01-04 14:31:11 -0500

    post data

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const http = new easyHTTP;
 
-const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(error, response){
-    console.log(response);
+// const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(error, response){
+//     console.log(response);
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log(response);
+//     }
+// });
+// console.log(posts);
+
+// const post = http.get('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
+//     console.log(response);
+//     if(error) {
+//         console.log(error);
+//     } else {
+//         console.log(response);
+//     }
+// });
+
+const data = {
+    title: 'Custom Post', 
+    body: 'This is a custom post'
+};
+
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post){
     if(error) {
         console.log(error);
     } else {
-        console.log(response);
+        console.log(post);
     }
 });
-console.log(posts);
--- http.js
+++ http.js
@@ -16,5 +16,14 @@ easyHTTP.prototype.get = function(url, callback) {
     this.http.send();
 }
 //HTTP POST REQUEST
+easyHTTP.prototype.post = function(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
+    this.http.send(JSON.stringify(data));
+}
 //HTTP PUT REQUEST
 //HTTP DELETE REQUEST

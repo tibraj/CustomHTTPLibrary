30eb5b7c0eb64cec395ec2f9e3344eaea2e9057a
tibraj <tibraj94@gmail.com>
2021-01-04 13:57:27 -0500

    handling errors

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const http = new easyHTTP;
 
-const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(response){
+const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(error, response){
     console.log(response);
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(response);
+    }
 });
 console.log(posts);

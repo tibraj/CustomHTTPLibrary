42d7e9a74112eec48b53eeb687fc919d86c6a3ee
tibraj <tibraj94@gmail.com>
2021-01-06 10:54:45 -0500

    es6 get request

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
-const http = new easyHTTP;
+// const http = new easyHTTP;
 
-// const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(error, response){
+// // const posts = http.get('https://jsonplaceholder.typicode.com/posts', function(error, response){
+// //     console.log(response);
+// //     if(error) {
+// //         console.log(error);
+// //     } else {
+// //         console.log(response);
+// //     }
+// // });
+// // console.log(posts);
+
+// // const post = http.get('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
+// //     console.log(response);
+// //     if(error) {
+// //         console.log(error);
+// //     } else {
+// //         console.log(response);
+// //     }
+// // });
+
+// const data = {
+//     title: 'Custom Post', 
+//     body: 'This is a custom post'
+// };
+
+// // http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post){
+// //     if(error) {
+// //         console.log(error);
+// //     } else {
+// //         console.log(post);
+// //     }
+// // });
+
+// // http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(error, post) {
+// //     if(error) {
+// //         console.log(error);
+// //     } else {
+// //         console.log(post);
+// //     }
+// // });
+
+// http.delete('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
 //     console.log(response);
 //     if(error) {
 //         console.log(error);
@@ -8,45 +48,7 @@ const http = new easyHTTP;
 //         console.log(response);
 //     }
 // });
-// console.log(posts);
 
-// const post = http.get('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
-//     console.log(response);
-//     if(error) {
-//         console.log(error);
-//     } else {
-//         console.log(response);
-//     }
-// });
-
-const data = {
-    title: 'Custom Post', 
-    body: 'This is a custom post'
-};
-
-// http.post('https://jsonplaceholder.typicode.com/posts', data, function(error, post){
-//     if(error) {
-//         console.log(error);
-//     } else {
-//         console.log(post);
-//     }
-// });
-
-// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(error, post) {
-//     if(error) {
-//         console.log(error);
-//     } else {
-//         console.log(post);
-//     }
-// });
-
-http.delete('https://jsonplaceholder.typicode.com/posts/1', function(error, response){
-    console.log(response);
-    if(error) {
-        console.log(error);
-    } else {
-        console.log(response);
-    }
-});
+const http = new EasyHTTP;
 
 
--- http.js
+++ http.js
@@ -50,7 +50,10 @@
 // }
 
 class EasyHTTP {
-    get() {
-        
+    get(url) {
+        fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
     }
 }

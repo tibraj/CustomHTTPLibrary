7520b3f8234b9ab2d09a6aa165392172f9db86d0
tibraj <tibraj94@gmail.com>
2021-01-06 11:25:31 -0500

    put request with async/await

--- app.js
+++ app.js
@@ -61,13 +61,13 @@ const data = {
 }
 
 
-http.post('https://jsonplaceholder.typicode.com/users', data)
-    .then(data => console.log (data))
-    .catch(err => console.log(err));
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log (data))
+//     .catch(err => console.log(err));
 
-// http.put('https://jsonplaceholder.typicode.com/users/2', data)
-// .then(data => console.log (data))
-// .catch(err => console.log(err));
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+.then(data => console.log (data))
+.catch(err => console.log(err));
 
 // http.delete('https://jsonplaceholder.typicode.com/users/2')
 // .then(data => console.log (data))
--- http.js
+++ http.js
@@ -68,19 +68,16 @@ class EasyHTTP {
             return resData
     }
 
-    put(url, data) {
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method: 'PUT',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err));
+    async put(url, data) {
+        const response = await fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
         });
+        const resData = await response.json();
+        return resData
     }
 
     delete(url) {
